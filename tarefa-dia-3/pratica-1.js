a4fe9601d777c43b0a2844f07e907af805c919e4
FabioL-96 <fabiolucio21@outlook.com.br>
2022-05-19 10:21:38 -0300

    Add variáveis

---
+++ tarefa-dia-3/pratica-1.js
@@ -0,0 +1,13 @@
+let nome = 'Fábio Lúcio Barrozo'
+let email = 'fabiolucio21@outlook.com.br'
+let profissão = 'Gerente'
+let idade = 25
+let lugardossonhos = 'Inglaterra'
+let mensagem = 'Olá, '+nome+'! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para '+lugardossonhos+'. Confirma pra gente alguns dados? A sua idade é '+idade+', você é '+profissão+' e o seu email é '+email+' ? Ficamos no aguardo. Muito obrigado e boa viagem! :D'
+
+console.log (nome)
+console.log(email)
+console.log(profissão)
+console.log(idade)
+console.log(lugardossonhos)
+console.log(mensagem)
