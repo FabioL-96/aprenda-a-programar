ebc73ea9be8926003a0e9c3a31892daccfb6679c
FabioL-96 <fabiolucio21@outlook.com.br>
2022-05-19 19:15:28 -0300

    Passo4

--- tarefa-dia-3/pratica-4.js
+++ tarefa-dia-3/pratica-4.js
@@ -2,17 +2,16 @@
 let produtoA = {"nome": "camiseta", "valor": 100.00, "internacional": true}
 let produtoB = {"nome": "perfume", "valor": 200.00, "internacional": true}
 let produtoC = {"nome": "sandália", "valor": 120.00, "internacional": false}
-let impnac= produtoA.valor * 1.12
-let impint= produtoA.valor * 1.20
-let estoque = [produtoA, produtoB, produtoC]
+let impnac= 1.12
+let impint= 1.2
 
-console.log(estoque)
 
-//Verificar se o produto é nacional ou internacional
-if (estoque.internacional == true) {
-    console.log('R$ '+impint+'');
+
+//Verificar se o produto é nacional ou internacional, e mostrar o valor com imposto
+if (produtoC.internacional == true) {
+    console.log ('Produto Internacional - R$ '+produtoC.valor * impint+'')
 } else {
-    console.log("Não")
+    console.log('Produto Nacional - R$ '+produtoC.valor * impnac+'')
 }
 
 
@@ -30,20 +29,13 @@ if (estoque.internacional == true) {
 
 
 
-//Usamos o condiconal para testar se a chave internacional do mapa produtoA é true ou false.
-if (produtoB.internacional == true) {
-    //para deixar o código organizado, podemos clarar uma variável que recebe o resultado do calculo de impostos. 
-    //Isso deixa o progrmaa mais legível sobre o que está acontecendo.
-   let valorImpostos = produtoC.valor * 1.2
-    //Podemos adiconar um console.log com uma mensagem que será útil para verificarmos se o programa funcionou corretamente.
-   console.log("O produto é internacional")
-    console.log(valorImpostos)
 
-} else {
-    let valorImpostos = produtoC.valor * 1.12
-    console.log("O produto é nacional")
-    console.log(valorImpostos)   
-}
+
+
+
+
+
+
 
 
 
